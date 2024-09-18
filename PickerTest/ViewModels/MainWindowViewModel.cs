2ecaace9eb8db383372146ae895d72c4db05f4a9
Victoria Ramírez Charles <vramirez@truenorth.pr>
2024-09-18 11:54:58 -0400

    Add project files.

---
+++ PickerTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+
+namespace PickerTest.ViewModels
+{
+    public partial class MainWindowViewModel: ObservableObject
+    {
+
+        [ObservableProperty]
+        public List<string> names =
+        [
+           "Alexander",
+            "Luis",
+            "Miguel",
+            "Javier",
+            "Carlos",
+            "Juan",
+            "Pedro",
+            "Jose",
+            "Ricardo",
+            "Fernando",
+            "Jorge",
+            "Alberto",
+        ];
+    }
+}
